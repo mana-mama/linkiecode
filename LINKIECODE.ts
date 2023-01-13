591256c19328c13fc44034ffbc2b2504e7fdbc8e
ppoks <122524277+ppoks@users.noreply.github.com>
2023-01-13 15:10:30 +0700

    Update pxt.json, LINKIECODE.ts

--- LINKIECODE.ts
+++ LINKIECODE.ts
@@ -4,5 +4,22 @@
 //% weight=100 color=#3ADADA icon="LC"
 
 namespace custom {
+    export enum Move {
+        //% block="Rest"
+        Rest,
+        //% block="Sit"
+        Sit,
+        //% block="Sleep"
+        Sleep,
+        //% block="Hello"
+        Hello,
+        //% block="Walk"
+        Walk
+    }
 
+    //% blockId=show_moves
+    //% block="$first"
+    export function moves(first: Move){
+
+    }
 }
